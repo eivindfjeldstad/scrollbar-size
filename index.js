c44b043990793c27d28f61d9092fdccfa94100f2
Eivind Fjeldstad <eivind.fjeldstad@gmail.com>
2013-07-12 17:44:22 +0200

    typo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var div = document.createElement('div');
 div.style.width = '50px';
 div.style.height = '50px';
 div.style.overflow = 'scroll';
-div.style.position: 'absolute';
+div.style.position = 'absolute';
 div.style.top = '-9999px';
 
 document.body.appendChild(div);
